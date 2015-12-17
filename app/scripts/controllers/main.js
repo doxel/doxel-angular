c003ecf97997c0aeee8885ad7e4e096f32606586
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2015-12-17 19:57:32 +0700

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name doxelAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the doxelAngularApp
+ */
+angular.module('doxelAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
