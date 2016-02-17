4f6c1510e9c89ee2b3fd8e6210bfc1204f1da239
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2016-02-18 03:53:57 +0700

    format-timestamp.js

---
+++ app/scripts/filters/format-timestamp.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name doxelApp.filter:formatTimestamp
+ * @function
+ * @description
+ * # formatTimestamp
+ * Filter in the doxelApp.
+ */
+angular.module('doxelApp')
+  .filter('formatTimestamp', function () {
+    return function (timestamp,format) {
+      var date=new Date(Number(timestamp.substr(0,10)+'000'));
+      var mm=date.getMonth()+1;
+      var dd=date.getDate();
+      var hh=date.getHours();
+      var mi=date.getMinutes();
+      var ss=date.getSeconds();
+      if (mm<10) mm='0'+mm;
+      if (dd<10) dd='0'+dd;
+      if (hh<10) hh='0'+hh;
+      if (mi<10) mi='0'+mi;
+      if (ss<10) ss='0'+ss;
+      switch(format) {
+        case 'hms':
+          return hh+':'+mi+':'+ss;
+        default:
+          return date.getFullYear()+'/'+mm+'/'+dd+', '+hh+':'+mi+':'+ss;
+      }
+    };
+  });
