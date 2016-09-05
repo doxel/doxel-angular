7821f53b9bba469b7eea10800f05c4eeee7716d6
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2016-09-05 20:24:07 +0200

    add license

--- app/scripts/controllers/gallery-viewer.js
+++ app/scripts/controllers/gallery-viewer.js
@@ -1,3 +1,38 @@
+/*
+ * gallery-viewer.js
+ *
+ * Copyright (c) 2015-2016 ALSENET SA - http://doxel.org
+ * Please read <http://doxel.org/license> for more information.
+ *
+ * Author(s):
+ *
+ *      Rurik Bogdanov <rurik.bugdanov@alsenet.com>
+ *
+ * This file is part of the DOXEL project <http://doxel.org>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ *
+ *      You are required to attribute the work as explained in the "Usage and
+ *      Attribution" section of <http://doxel.org/license>.
+ */
+
 'use strict';
 
 /**
@@ -19,7 +54,7 @@ angular.module('doxelApp')
     angular.extend($scope,{
       viewer_state: 'gallery.view.cloud',
 
-      updateViewerState: function(state) {
+      updateViewerVisibility: function(state) {
          console.log(state)
         if (state==$scope.viewer_state) {
           $scope.viewer_visible=true;
@@ -31,10 +66,10 @@ angular.module('doxelApp')
       },
 
       init: function(){
-        $scope.updateViewerState($state.current.name);
+        $scope.updateViewerVisibility($state.current.name);
 
         $rootScope.$on('$stateChangeSuccess',function(event,toState){
-          $scope.updateViewerState(toState.name);
+          $scope.updateViewerVisibility(toState.name);
         });
 
         $scope.$on('segment.clicked',function(event,segment){
