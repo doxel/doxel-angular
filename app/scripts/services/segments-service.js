1f8023c0109a59e69b5a46f7db5e3c6512f5cd2b
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2017-01-22 19:36:22 +0100

    add segments-service.js

---
+++ app/scripts/services/segments-service.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name doxelApp.segmentsService
+ * @description
+ * # segmentsService
+ * Service in the doxelApp.
+ */
+angular.module('doxelApp')
+  .service('segmentsService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    var segmentsService=this;
+    angular.extend(segmentsService,{
+      segments: []
+    });
+
+  });
