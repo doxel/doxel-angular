9e67dc7853461fdd20ea60dec43bc2c56ca1d941
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2017-01-22 17:45:00 +0100

    update gallery.js
    - move segments property in segmentService.segments
    - load max 16 segments thumbs at a time
    - sort segments when loading backwards

--- app/scripts/controllers/gallery.js
+++ app/scripts/controllers/gallery.js
@@ -43,258 +43,275 @@
  * Controller of the doxelApp
  */
 angular.module('doxelApp')
-  .controller('GalleryCtrl', function ($scope,$rootScope,$state,errorMessage,Segment,$timeout,$location,$q,appConfig) {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-    this.thumbsWidth=200;
+  .controller('GalleryCtrl', [
+    '$scope',
+    '$rootScope',
+    '$state',
+    'errorMessage',
+    'Segment',
+    '$timeout',
+    '$location',
+    '$q',
+    'appConfig',
+    'segmentsService',
+    function ($scope,$rootScope,$state,errorMessage,Segment,$timeout,$location,$q,appConfig,segmentsService) {
+      this.awesomeThings = [
+        'HTML5 Boilerplate',
+        'AngularJS',
+        'Karma'
+      ];
+      this.thumbsWidth=200;
 
-    angular.extend($scope,{
-      end: {},
-      loaded: {},
-      segments: [],
-      selected: {},  // unloaded segment could be selected because of paging or page reload. TODO: restore selection on load
-      center: [0,0],
-      init: function(){
+      angular.extend($scope,{
+        end: {},
+        loaded: {},
+        segments: segmentsService.segments,
+        selected: {},  // unloaded segment could be selected because of paging or page reload. TODO: restore selection on load
+        center: [0,0],
+        init: function(){
 
-        $scope.updateMetrics();
+          $scope.updateMetrics();
 
-        //// on state change success
-        $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+          //// on state change success
+          $rootScope.$on('$stateChangeSuccess', function (event, toState) {
+            // scope is visible when beginning with 'gallery'
+            $scope.visible=(toState.name.substr(0,7)=='gallery');
+            if (!$scope.visible) return;
+
+            $scope.updateMetrics(toState);
+
+            var params=$rootScope.params;
+
+            // when state is "gallery", switch gallery.view.thumbs
+
+            // timeout is needed so that ui-leaflet display and behave properly,
+            // when returning to gallery from other tabs
+            if (toState.name=='gallery')
+            $timeout(function(){
+              $state.go('gallery.view.thumbs',{},{
+                location: 'replace'
+              });
+            },1)
+
+          });
+
+          //// onload
           // scope is visible when beginning with 'gallery'
-          $scope.visible=(toState.name.substr(0,7)=='gallery');
-          if (!$scope.visible) return;
-
-          $scope.updateMetrics(toState);
-
-          var params=$rootScope.params;
-
-          // when state is "gallery", switch gallery.view.thumbs
-
-          // timeout is needed so that ui-leaflet display and behave properly,
-          // when returning to gallery from other tabs
-          if (toState.name=='gallery')
-          $timeout(function(){
-            $state.go('gallery.view.thumbs',{},{
+          $scope.visible=($state.current.name.substr(0,7)=='gallery');
+          if ($state.current.name=='gallery') {
+            $state.go('gallery.view.thumbs',{
               location: 'replace'
             });
-          },1)
-
-        });
-
-        //// onload
-        // scope is visible when beginning with 'gallery'
-        $scope.visible=($state.current.name.substr(0,7)=='gallery');
-        if ($state.current.name=='gallery') {
-          $state.go('gallery.view.thumbs',{
-            location: 'replace'
-          });
-        }
-
-        var segmentId=$rootScope.params.s||$location.search().s;
-        if (segmentId) {
-        /*  $scope.getSegment(segmentId).then(function(segment){
-            $timeout(function(){
-              segment.selected=true;
-              $scope.broadcast('showThumb',segmentId);
-            },1000);
-          });
-*/
-        } else {
-          $scope.loadSegments();
-        }
-
-      }, // init
-
-      updateMetrics: function(state){
-console.log('updateMetrics');
-        $scope.thumbsH=Math.floor($('#gallery').width()/200);
-        $scope.thumbsV=Math.round(($('#gallery .mCustomScrollbar').height()||$('body').height())/150);
-        switch($scope.thumbsPosition) {
-          case 'thumbs-left': $scope.thumbsH=1; break;
-          case 'thumbs-bottom': $scope.thumbsV=1; break;
-        }
-        $scope.maxThumbs=Math.floor($scope.thumbsH * $scope.thumbsV ) || 8;
-        while($scope.maxThumbs<8) $scope.maxThumbs*=2;
-      },
-
-      getLimit: function(){
-        return Math.round($scope.thumbsH*$scope.thumbsV*1.5)||12;
-      },
-
-      loadSegments: function(direction,count) {
-        $scope.updateMetrics();
-        console.trace('loadSegments');
-        if ($scope._loadSegments) {
-          return $scope._loadSegments;
-        }
-        $scope._loadSegments=$q.defer();
-        $scope._loadSegments.promise.finally(function(){
-          $scope._loadSegments=null;
-        });
-
-        direction=direction||'forward';
-
-        if ($scope.end[direction]) {
-          // end reached
-          $scope._loadSegments.resolve([]);
-          return $scope._loadSegments;
-        }
-
-        var filter={
-          where: {
-             pointCloudId: {exists: true}
-          },
-          include: 'pointCloud',
-          limit: count || $scope.getLimit()
-        }
-
-        filter.limit+=$scope.thumbsH-((($scope.segments.length + filter.limit) % $scope.thumbsH ));
-
-        console.log('limit',filter.limit)
-
-        if (direction=='forward') {
-          // load chunk after last segment in $scope.segments
-          if ($scope.segments.length) {
-            var segment=$scope.segments[$scope.segments.length-1];
-            filter.where.timestamp={
-              lte: segment.timestamp
-            };
-            filter.where.id={
-              neq: segment.id
-            };
           }
-          filter.order='timestamp DESC';
 
-        } else {
-          // load chunk before first segment in $scope.segments
-          if ($scope.segments.length) {
-            var segment=$scope.segments[0];
-            filter.where.timestamp={
-              gte: segment.timestamp
-            };
-            filter.where.id={
-              neq: segment.id
-            };
+          var segmentId=$rootScope.params.s||$location.search().s;
+          if (segmentId) {
+          /*  $scope.getSegment(segmentId).then(function(segment){
+              $timeout(function(){
+                segment.selected=true;
+                $scope.broadcast('showThumb',segmentId);
+              },1000);
+            });
+  */
+          } else {
+            $scope.loadSegments();
           }
-          filter.order='timestamp ASC';
-        }
 
-        // do load segments
-        Segment.find({
-          filter: filter
+        }, // init
 
-        }, function(segments){
-          if (!segments || !segments.length) {
+        updateMetrics: function(state){
+  console.log('updateMetrics');
+          $scope.thumbsH=Math.floor($('#gallery').width()/200);
+          $scope.thumbsV=Math.round(($('#gallery .mCustomScrollbar').height()||$('body').height())/150);
+          switch($scope.thumbsPosition) {
+            case 'thumbs-left': $scope.thumbsH=1; break;
+            case 'thumbs-bottom': $scope.thumbsV=1; break;
+          }
+          $scope.maxThumbs=Math.floor($scope.thumbsH * $scope.thumbsV ) || 8;
+          while($scope.maxThumbs<8) $scope.maxThumbs*=2;
+        },
+
+        getLimit: function(){
+          return Math.min(16,Math.round($scope.thumbsH*$scope.thumbsV*1.5)||12);
+        },
+
+        loadSegments: function(direction,count) {
+          $scope.updateMetrics();
+          console.trace('loadSegments');
+          if ($scope._loadSegments) {
+            return $scope._loadSegments;
+          }
+          $scope._loadSegments=$q.defer();
+          $scope._loadSegments.promise.finally(function(){
+            $scope._loadSegments=null;
+          });
+
+          direction=direction||'forward';
+
+          if ($scope.end[direction]) {
             // end reached
-            $scope.end[direction]=true;
+            $scope._loadSegments.resolve([]);
+            return $scope._loadSegments;
+          }
+
+          var filter={
+            where: {
+               pointCloudId: {exists: true}
+            },
+            include: 'pointCloud',
+            limit: count || $scope.getLimit()
+          }
+
+          filter.limit+=$scope.thumbsH-((($scope.segments.length + filter.limit) % $scope.thumbsH ));
+
+          console.log('limit',filter.limit)
+
+          if (direction=='forward') {
+            // load chunk after last segment in $scope.segments
+            if ($scope.segments.length) {
+              var segment=$scope.segments[$scope.segments.length-1];
+              filter.where.timestamp={
+                lte: segment.timestamp
+              };
+              filter.where.id={
+                neq: segment.id
+              };
+            }
+            filter.order='timestamp DESC';
 
           } else {
-            if (direction=='backward') {
-              // prepend segments
-              segments.reverse();
-              angular.forEach(segments,function(segment){
-                if (!$scope.loaded[segment.id]) {
-                  $scope.segments.unshift(segment);
-                  $scope.loaded[segment.id]=true;
-                }
-              });
-
-            } else { // direction==forward
-              // append segments
-              angular.forEach(segments,function(segment){
-                if (!$scope.loaded[segment.id]) {
-                  $scope.segments.push(segment);
-                  $scope.loaded[segment.id]=true;
-                }
-              });
+            // load chunk before first segment in $scope.segments
+            if ($scope.segments.length) {
+              var segment=$scope.segments[0];
+              filter.where.timestamp={
+                gte: segment.timestamp
+              };
+              filter.where.id={
+                neq: segment.id
+              };
             }
+            filter.order='timestamp ASC';
           }
-          $('#segments .mCustomScrollbar').mCustomScrollbar('update');
-          $scope._loadSegments.resolve(segments);
-//          $scope._loadSegments.promise.then(function(segments){
-//            $rootScope.$broadcast('segments-loaded',segments);
-//          });
 
-        }, function(err){
-          console.log(err);
-          errorMessage.show('Could not load segments');
-          $scope._loadSegments.reject(err);
-        });
+          // do load segments
+          Segment.find({
+            filter: filter
 
-        return $scope._loadSegments;
+          }, function(segments){
+            if (!segments || !segments.length) {
+              // end reached
+              $scope.end[direction]=true;
 
-      }, // loadSegments
+            } else {
+              if (direction=='backward') {
+                // prepend segments
+                segments.reverse();
+                angular.forEach(segments,function(segment){
+                  $scope.segments.some(function(_segment,i){
+                    if ($scope.loaded[segment.id]) {
+                      return false;
+                    }
+                    if (segment.timestamp>_segment.timestamp) {
+                      $scope.segments.splice(i,0,segment);
+                      $scope.loaded[segment.id]=true;
+                      return false;
+                    }
+                  });
+                });
 
-      getSegment: function(segmentId){
-        console.trace('getSegment');
-          var q=$q.defer();
-          var found;
-          // search in loaded segments
-          $scope.segments.some(function(segment){
-            if (segment.id==segmentId) {
-              found=true;
-              q.resolve(segment);
-              return true;
-            }
-          });
-          if (!found) {
-            // currently loading segments ?
-            if ($scope._loadSegments && $scope._loadSegments.promise.$$state.pending) {
-              // search in segment being loaded
-              $scope._loadSegments.promise.then(function(_segments){
-                _segments.some(function(segment){
-                  if (segment.id==segmentId) {
-                    found=true;
-                    q.resolve(segment);
-                    return true;
+              } else { // direction==forward
+                // append segments
+                angular.forEach(segments,function(segment){
+                  if (!$scope.loaded[segment.id]) {
+                    $scope.segments.push(segment);
+                    $scope.loaded[segment.id]=true;
                   }
                 });
-                if (!found) {
-                  // or finally load the required segment
-                  $scope.loadSegmentsAround(segmentId,function(segment){
-                    q.resolve(segment);
-                  },q.reject);
-                }
-              });
-            } else {
-              // or finally load the required segment
-              $scope.loadSegmentsAround(segmentId,function(segment){
-                q.resolve(segment);
-              },q.reject);
+              }
             }
-          }
-          return q.promise;
-      }, // getSegment
+            $('#segments .mCustomScrollbar').mCustomScrollbar('update');
+            $scope._loadSegments.resolve(segments);
+  //          $scope._loadSegments.promise.then(function(segments){
+  //            $rootScope.$broadcast('segments-loaded',segments);
+  //          });
 
-      loadSegmentsAround: function(segmentId,_then,_catch) {
-        console.trace('loadSegmentsAround');
-        Segment.findById({
-          id: segmentId,
-          filter: {
-            include: 'pointCloud'
-          }
-        }, function(segment){
-          if (segment) {
-            Array.prototype.splice.apply($scope.segments,[0,$scope.segments.length,segment]);
-            $scope.loadSegments('backward').promise.then(function(){
-              $timeout(function(){
-                $scope.loadSegments('forward').promise.then(function(){
-                  _then(segment);
-                },_catch);
-              });
-            },_catch);
-          } else {
-            _catch(new Error('Segment not found !'));
-          }
-        },_catch);
+          }, function(err){
+            console.log(err);
+            errorMessage.show('Could not load segments');
+            $scope._loadSegments.reject(err);
+          });
 
-      } // loadSegmentsAround
+          return $scope._loadSegments;
 
-    });
+        }, // loadSegments
 
-    $scope.init();
+        getSegment: function(segmentId){
+          console.trace('getSegment');
+            var q=$q.defer();
+            var found;
+            // search in loaded segments
+            $scope.segments.some(function(segment){
+              if (segment.id==segmentId) {
+                found=true;
+                q.resolve(segment);
+                return true;
+              }
+            });
+            if (!found) {
+              // currently loading segments ?
+              if ($scope._loadSegments && $scope._loadSegments.promise.$$state.pending) {
+                // search in segment being loaded
+                $scope._loadSegments.promise.then(function(_segments){
+                  _segments.some(function(segment){
+                    if (segment.id==segmentId) {
+                      found=true;
+                      q.resolve(segment);
+                      return true;
+                    }
+                  });
+                  if (!found) {
+                    // or finally load the required segment
+                    $scope.loadSegmentsAround(segmentId,function(segment){
+                      q.resolve(segment);
+                    },q.reject);
+                  }
+                });
+              } else {
+                // or finally load the required segment
+                $scope.loadSegmentsAround(segmentId,function(segment){
+                  q.resolve(segment);
+                },q.reject);
+              }
+            }
+            return q.promise;
+        }, // getSegment
 
-  });
+        loadSegmentsAround: function(segmentId,_then,_catch) {
+          console.trace('loadSegmentsAround');
+          Segment.findById({
+            id: segmentId,
+            filter: {
+              include: 'pointCloud'
+            }
+          }, function(segment){
+            if (segment) {
+              Array.prototype.splice.apply($scope.segments,[0,$scope.segments.length,segment]);
+              $scope.loadSegments('backward').promise.then(function(){
+                $timeout(function(){
+                  $scope.loadSegments('forward').promise.then(function(){
+                    _then(segment);
+                  },_catch);
+                });
+              },_catch);
+            } else {
+              _catch(new Error('Segment not found !'));
+            }
+          },_catch);
+
+        } // loadSegmentsAround
+
+      });
+
+      $scope.init();
+
+    }]);
