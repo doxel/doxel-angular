b8d5dbb169cf10045f30aadfc37ecd4ebffe4119
Rurik Bogdanov <rurik.bugdanov@alsenet.com>
2017-01-25 22:09:37 +0000

    Gruntfile: disable jscs for speed

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,14 +39,14 @@ module.exports = function (grunt) {
       },
       js: {
         files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-        tasks: ['newer:jshint:all', 'newer:jscs:all'],
+        tasks: ['newer:jshint:all'/*, 'newer:jscs:all'*/],
         options: {
           livereload: '<%= connect.options.livereload %>'
         }
       },
       jsTest: {
         files: ['test/spec/{,*/}*.js'],
-        tasks: ['newer:jshint:test', 'newer:jscs:test'/*, 'karma'*/]
+        tasks: ['newer:jshint:test'/*, 'newer:jscs:test', 'karma'*/]
       },
       compass: {
         files: ['<%= yeoman.app %>/styles/{,*/}*.{scss,sass}'],
@@ -512,7 +512,7 @@ module.exports = function (grunt) {
 
   grunt.registerTask('default', [
     'newer:jshint',
-    'newer:jscs',
+//    'newer:jscs',
     'test',
     'build'
   ]);
