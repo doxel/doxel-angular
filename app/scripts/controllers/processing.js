8839d82c451b3cba4dfa85d9ded4df19e759b5fb
Rurik Bugdanov <rurik.bugdanov@alsenet.com>
2017-08-30 10:53:57 +0200

    WIP processing.js

--- app/scripts/controllers/processing.js
+++ app/scripts/controllers/processing.js
@@ -8,6 +8,49 @@
  * Controller of the doxelApp
  */
 angular.module('doxelApp')
+.directive('lrInfiniteScroll', ['$timeout', function ($timeout) {
+        return{
+            link: function (scope, element, attr) {
+                var
+                    lengthThreshold = attr.scrollThreshold || 250,
+                    timeThreshold = attr.timeThreshold || 0,
+                    handler = scope.$eval(attr.lrInfiniteScroll),
+                    promise = null,
+                    lastRemaining = 9999;
+
+                lengthThreshold = parseInt(lengthThreshold, 10);
+                timeThreshold = parseInt(timeThreshold, 10);
+
+                if (!handler || !angular.isFunction(handler)) {
+                    handler = angular.noop;
+                }
+
+                element.bind('scroll', function () {
+                    lengthThreshold = parseInt(attr.scrollThreshold) || element[0].clientHeight/3;
+
+                    var
+                        remaining = element[0].scrollHeight - (element[0].clientHeight + element[0].scrollTop);
+                        console.log(remaining);
+
+                    //if we have reached the threshold and we scroll down
+                    if (remaining < lengthThreshold && (remaining - lastRemaining) < 0) {
+
+                        //if there is already a timer running which has no expired yet we have to cancel it and restart the timer
+                        if (promise !== null) {
+                          return;
+                        }
+                          promise = handler();
+                          console.log(promise);
+                          promise.then(function(){
+                              promise = null;
+                          });
+                    }
+                    lastRemaining = remaining;
+                });
+            }
+
+        };
+    }])
 .controller('ProcessingCtrl', [
   '$scope',
   '$rootScope',
@@ -40,14 +83,13 @@ angular.module('doxelApp')
       segmentsVisible: [],
       picturesVisible: [],
       itemsPerPage: 18,
-
+/*
       verticalScrollConfig: {
-        axis: 'y',
         theme: 'light',
         scrollButtons: {
           enable: false
         },
-        mouseWheel:{ preventDefault: true },
+        mouseWheel:{ preventDefault: false },
         advanced: {
           updateOnContentResize: true
         },
@@ -59,17 +101,22 @@ angular.module('doxelApp')
           onTotalScroll: function(){
             return $scope.onTotalScroll.apply(this,Array.prototype.slice.call(arguments));
           },
-          onTotalScrollOffset: 60,
+          onTotalScrollOffset: 120,
           alwaysTriggerOffsets: false,
 
         }
 
       }, // verticalScrollConfig
-
+*/
       initEventHandlers: function(){
         $scope.$on('$locationChangeSuccess',function($event,newUrl,oldUrl,newState,oldState){
           console.log(arguments);
         });
+
+        $scope.$on('picture.click', function($event,picture){
+    //      $scope.picture=picture;
+        });
+
       },
 
       init: function() {
@@ -80,6 +127,9 @@ angular.module('doxelApp')
         $scope.loadingProgress=0;
         $scope.loadSegments()
           .then(function(segments){
+            segments=loopbackFilters(segments,{
+              order: 'created DESC'
+            });
             $scope.segments=segments;
             // load and format segments data (must all be done at once for sortiing)
             segments.reduce(function(promise, segment){
@@ -102,9 +152,9 @@ angular.module('doxelApp')
         return Segment.find({
           filter: {
             where: {
-              pointCloudId: {exists: false}
+              status: {ne: 'published'}
             },
-            limit: 21,
+//            limit: 2,
             fields: {
               id: true,
               previewId: true,
@@ -118,8 +168,18 @@ angular.module('doxelApp')
         }).$promise;
       },
 
+      // get segment data to be displayed
       getSegmentData: function(segment) {
+        // format timestamp
         segment.timestamp_str=formatTimestampFilter(segment.timestamp,'ymdhm');
+
+        // set status
+        switch (segment.status) {
+          case undefined:
+            segment.status="new";
+            break;
+        }
+
         var q=$q.defer();
         var todo=2;
         function decr(){
@@ -143,34 +203,55 @@ angular.module('doxelApp')
           console.log('no userId',segment);
           return $q.reject();
         }
+        // use cached user info when available
         if (global.usersCache[userId]) {
           segment.user=global.usersCache[userId];
           return $q.resolve();
         }
         return User.findById({id: userId},function(user){
+          // store user info in cache
           global.usersCache[userId]==user;
           segment.user=user;
+
+          // prepend char to anonymous username (id) for sorting
           if (user.username.length==32 && user.username.match(/^[a-z0-9]+$/)) {
             segment._username='||'+user.username;
           } else {
             segment._username=user.username;
           }
+
         }, function(err){
           console.log(err);
         }).$promise;
       },
 
+      // display segment pictures
       viewSegment: function(segment){
         if (!segment.pictures) {
+
+          // load picture ids
           segment.pictures_promise=Segment.pictures({
             id: segment.id,
             fields: {
               id: true
             }
+
           }, function(pictures){
+
             segment.pictures=pictures;
+
+            // initialize picture ids pool
             $scope.picturesPool=pictures;
-            $scope.picturesVisible=pictures.slice(0,18);
+
+            // initialize visible pictures
+            $scope.picturesVisible=pictures.slice(0,$scope.itemsPerPage);
+
+            // set picture label
+            pictures.some(function(picture){
+              picture.label=picture.timestamp;
+            });
+
+            $scope.fillScrollableContainer();
 
           }, function(err){
             console.log(err);
@@ -184,19 +265,124 @@ angular.module('doxelApp')
 
       },
 
-      onTotalScroll: function() {
-        console.log(arguments);
-        $scope.getPictures();
+      fillScrollableContainer: function() {
+        function loop() {
+          if ($scope.picturesVisible.length<$scope.picturesPool.length && !$scope.isScrollbarVisible()){
+            $scope.getPictures();
+          }
+          $timeout(loop,2000);
+        }
+        loop();
       },
 
+      isScrollbarVisible: function(){
+        return $('.thumbs').height>$('.pictures').height();
+
+      //  return ($('#processing .pictures .mCSB_scrollTools_vertical:visible').length != 0);
+      },
+
+/*
+      onTotalScroll: function() {
+        $scope.getPictures();
+      },
+*/
+
+      // Add pictures to visible pictures
       getPictures: function(tableState){
-        if (!$scope.segment) return;
+        if (!$scope.segment) return $q.resolve();
+        var q=$q.defer();
         $scope.segment.pictures_promise.then(function(){
-          $timeout(function(){
-            $scope.picturesVisible=$scope.picturesVisible.concat($scope.picturesPool.slice($scope.picturesVisible.length, $scope.picturesVisible.length+18));
-          });
+          $scope.picturesVisible=$scope.picturesVisible.concat($scope.picturesPool.slice($scope.picturesVisible.length, $scope.picturesVisible.length+$scope.itemsPerPage/2));
+          $timeout(q.resolve());
         });
-      }
+        return q.promise;
+      },
+
+      proceed: function(segment, direction){
+        return Segment.proceed({
+          id: segment.id,
+          status: segment.status,
+          direction: direction
+
+        }).$promise.then(function(res){
+          segment.status=res.status;
+        })
+        .catch(function(err){
+          console.log(err);
+          $scope.error=err;
+        });
+      },
+
+      proceedFurther: function(segment){
+        $scope.lock();
+        $scope.proceed(segment,'forward').finally($scope.unlock);
+      },
+
+      proceedNoFurther: function(segment){
+        $scope.lock();
+        $scope.proceed(segment,'backward').finally($scope.unlock);
+      },
+
+      // lock gui, ie display overlay
+      lock: function(){
+        $scope.locked=true;
+      },
+
+      unlock: function(){
+        $scope.locked=false;
+      },
+
+      isCheckButtonDisabled: function(segment){
+        return segment.status=='processing'||segment.status=='queued';
+      },
+
+      cancelButtonTitle: function(segment){
+        switch(segment.status) {
+          default:
+            console.log('unhandled status, assume discard');
+
+          case undefined:
+          case 'processed':
+            return 'Discard this segment';
+
+          case 'queued':
+            return 'Remove from queue';
+
+          case 'processing':
+            return 'Cancel processing';
+        }
+
+      },
+
+      cancelButtonClick: function(segment) {
+        switch(segment.status) {
+          default:
+            console.log('unhandled status, assume discard');
+
+          case undefined:
+          case 'processed':
+            proceedNoFurther(segment);
+            break;
+
+          case 'queued':
+            return 'Remove from queue';
+
+          case 'processing':
+            return 'Cancel processing';
+        }
+      },
+
+      proceedNoFurther: function(segment){
+        Segment.proceedNoFurther({
+          id: segment.id
+        }).$promise.then(function(res){
+          segment.status=res.status;
+        })
+        .catch(function(err){
+          console.log(err);
+          $scope.error=err;
+        });
+      },
 
 
 
