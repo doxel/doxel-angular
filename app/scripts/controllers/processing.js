a45e935d4685edd394c84aee0421464d3d3898ae
Rurik Bogdanov <rurik.bugdanov@alsenet.com>
2017-11-09 13:47:33 +0700

    processing view: add stPersist

--- app/scripts/controllers/processing.js
+++ app/scripts/controllers/processing.js
@@ -312,4 +312,33 @@ angular.module('doxelApp')
     $scope.init();
   }
 
-]);
+])
+// source: http://plnkr.co/edit/q6w0sOR8eKGc83fUnhFZ?p=preview
+.directive('stPersist', function () {
+        return {
+            require: '^stTable',
+            link: function (scope, element, attr, ctrl) {
+                var nameSpace = attr.stPersist;
+
+                //save the table state every time it changes
+                scope.$watch(function () {
+                    return ctrl.tableState();
+                }, function (newValue, oldValue) {
+                    if (newValue !== oldValue) {
+                        localStorage.setItem(nameSpace, JSON.stringify(newValue));
+                    }
+                }, true);
+
+                //fetch the table state when the directive is loaded
+                if (localStorage.getItem(nameSpace)) {
+                    var savedState = JSON.parse(localStorage.getItem(nameSpace));
+                    var tableState = ctrl.tableState();
+
+                    angular.extend(tableState, savedState);
+                    ctrl.pipe();
+
+                }
+
+            }
+        };
+    });;
