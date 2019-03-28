793e36cf7741accb753a6beb9a5e725d8f307853
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2019-03-28 16:27:05 +0100

    update segment-job-config.js

--- app/scripts/controllers/segment-job-config.js
+++ app/scripts/controllers/segment-job-config.js
@@ -10,16 +10,10 @@
 angular.module('doxelApp')
   .controller('SegmentJobConfigCtrl', [
     '$scope',
-    '$rootScope',
-    'Segment',
     '$stateParams',
-    '$q',
     function(
       $scope,
-      $rootScope,
-      Segment,
       $stateParams,
-      $q
     ) {
       this.awesomeThings = [
         'HTML5 Boilerplate',
@@ -30,59 +24,8 @@ angular.module('doxelApp')
       var self=this;
 
       angular.extend($scope, {
-        init: function() {
-          self.config_promise=$q.defer();
-
-          fetch('/job-config.json')
-          .then(function(response) {
-            return response.json()
-          }).then(self.config_promise.resolve)
-          .catch(function(ex) {
-            self.config_promise.reject(ex);
-            console.log('parsing failed', ex)
-          });
-
-          Segment.findById({
-            id:$scope.$stateParams.segmentId,
-            filter: {
-              include: 'jobs'
-            }
-          },{
-          }, function(segment){
-            $scope.segment=segment;
-            self.config_promise.promise.then(function(json){
-              $scope.model=angular.merge({},json.defaults,segment.params.jobConfig||{});
-              $scope.schema=json.schema;
-              $scope.form=[
-                "*",
-                {
-                  type: "actions",
-                  items: [
-                    { type: 'button', style: 'btn-success', title: 'Save', onClick: $scope.save },
-                    { type: 'button', style: 'btn-info', title: 'Cancel', onClick: $scope.cancel }
-                  ]
-                }
-              ];
-              $scope.visible=true;
-              $scope.$broadcast('schemaFormRedraw')
-
-            });
-          });
-        },
-
-        save: function() {
-          $scope.segment.params.jobConfig=$scope.model;
-          var promise=$scope.segment.$setJobConfig({jobConfig:$scope.model});
-          promise.then(console.log).catch(function(err){
-            window.alert("An error occured while trying to save the config");
-            console.error(err);
-          });
-        },
-
-        cancel: function() {
-          window.history.back();
-        }
-
+        showForm: true,
+        segmentId: $stateParams.segmentId
       });  // extend $scope
 
       $scope.init();
